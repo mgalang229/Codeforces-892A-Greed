bb240dc657c938339a8ca9cc17c038ca89894639
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-09-14 13:10:42 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	long long sum = 0;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		sum += a;
+	}
+	vector<int> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+	sort(b.begin(), b.end());
+	// check if total of volume of the all of the colas is can be accommodated by the 2 largest cans
+	cout << (sum <= (long long) b[n - 1] + b[n - 2] ? "YES" : "NO") << '\n';
+	return 0;
+}
